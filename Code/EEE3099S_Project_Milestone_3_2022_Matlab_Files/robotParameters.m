5f5405fd70912d9e1762ae855e1bbee6472722b4
Carciax <122102594+Carciax@users.noreply.github.com>
2023-10-02 12:36:45 +0200

    cleaned folder

--- Code/EEE3099S_Project_Milestone_3_2022_Matlab_Files/robotParameters.m
+++
@@ -1,8 +0,0 @@
-%Robot Parameters
-wheelR = 0.031; %Wheel radius (m)
-axleLength = 0.136; %Distance between wheels (m)
-ticksPerRot = 20; % Ticks per rotation for encoders
-load wheelLUT %Look up table for platform motors
-Ts = 0.01; %Sample time
-load wheelMotorModel %Motor model for platform motorswheelMotorModel
-load straight_line.mat
